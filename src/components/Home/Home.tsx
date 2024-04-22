7efbd19bb1729c3a3c80b47aa732a3ed70fed239
MehrShahbaz <mehrshahbaz7@gmail.com>
2024-04-22 19:32:47 +0200

    Implement Firebase login and logout wtih google

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,25 +1,14 @@
-import { useCallback } from 'react';
-import { useDispatch } from 'react-redux';
+import Navbar from '../_shared/Navbar/Navbar';
 
-import { logout } from '../../redux/slices/authSlice';
-import { AppDispatch } from '../../redux/store';
-
-const Home = (): JSX.Element => {
-  const dispatch = useDispatch<AppDispatch>();
-  const loggedOut = useCallback(() => {
-    dispatch(logout());
-  }, [dispatch]);
-
-  console.log(1);
-
-  return (
-    <div>
-      <div>Welcome To the App</div>
-      <button onClick={loggedOut} className="px-6 py-3 bg-violet-600 text-white rounded-lg hover:bg-violet-700">
-        Logout
-      </button>
-    </div>
-  );
-};
+const Home = (): JSX.Element => (
+  <div>
+    <Navbar />
+    <div>Welcome To the App</div>
+    {/* <img
+      src="https://lh3.googleusercontent.com/a/ACg8ocJc3xsogzXEHlp2eKg9vyPFqgzB4TyXW7gSCL3XtkZzUrlSM1ap=s96-c"
+      alt=""
+    /> */}
+  </div>
+);
 
 export default Home;
